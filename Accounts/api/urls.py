d22339804a936643b49de550b64bdb14a25acb8e
Walsh Fernandes <2016.walsh.fernandes@ves.ac.in>
2018-12-07 18:20:24 +0530

    minor changes

--- Accounts/api/urls.py
+++ Accounts/api/urls.py
@@ -4,7 +4,10 @@ from .views import User_Registration_API_View, User_Deactivate_API_View
 
 urlpatterns = [
     # authtoken view to retrieve the user token 
+    # username, password, <admin token> POST
     url(r'^login/$', obtain_auth_token, name='obtain_token'),
+    # username, password, first_name, last_name, email, phone_number POST
     url(r'^register/$',User_Registration_API_View.as_view(), name='register_user'),
+    # token of the user through POST 
     url(r'^deactivate/$', User_Deactivate_API_View.as_view(), name='deactivate'),
 ]
